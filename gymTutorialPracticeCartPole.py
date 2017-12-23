3d19e6b2a08f332bbafaa12867b77a434484798a
Andrew Winnicki <andrew.winnicki.sc@gmail.com>
2017-12-22 17:10:28 -1000

    imported code from me_pong.py from medium.com

--- gymTutorialPracticeCartPole.py
+++ gymTutorialPracticeCartPole.py
@@ -1,17 +1,24 @@
 import gym
 env = gym.make('CartPole-v0') # create the cart pole environment
 # create a number of episodes to go through
+# store the high score
+highscore = 0
 for nth_episode in range(20):
 	observation = env.reset() # create a new observation based on the environment in
-	for x in range(100):
+	points = 0 # keep track of the reward per episode
+	while True:
 	    env.render() # render the environment again after one new step
-	    print(observation) # prints the observations for debugging purposes
-	    action = env.action_space.sample() # random action
-	    print(action)
-	    observation, reward, done, info = env.step(action) # take a new step based on the randomly generated action
-	    if done: # check if done; if this is so, then print end of episode
-	    	print("finished after {} timesteps".format(x+1))
+	    action = 1 if observation[2] > 0 else 0 # if angle positive, move right, if neg, move left
+	    if observation[2] < -20 or observation[2] > 20:
 	    	break
+	    observation, reward, done, info = env.step(action) # take a new step based on the randomly generated action
+	    points += reward
+	    print(points)
+	    if done:
+	    	if points > highscore:
+	    		highscore = points
+		    	# check if done; if this is so, then set highscore = points and break to move to next episode
+		    	break
 
 
 # # from gym documentation itself
